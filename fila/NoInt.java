ddaabfd7cb417a9dbfa99d33fcea292ac914cb0e
Davi Oliveira <thisdevdavioliveira@gmail.com>
2025-07-22 21:12:46 -0300

    Class NoInt

---
+++ fila/NoInt.java
@@ -0,0 +1,32 @@
+package fila;
+
+public class NoInt {
+	private int elemento;
+	private NoInt prox;
+	
+	public NoInt(int elemento) {
+		this.elemento = elemento;
+		this.prox = null; 
+	}
+
+	public int getElemento() {
+		return elemento;
+	}
+
+	public void setElemento(int elemento) {
+		this.elemento = elemento;
+	}
+
+	public NoInt getProx() {
+		return prox;
+	}
+
+	public void setProx(NoInt prox) {
+		this.prox = prox;
+	}
+
+	@Override
+	public String toString() {
+		return "Elemento = "+elemento;
+	}
+}
